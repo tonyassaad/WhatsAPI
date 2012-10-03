a6e5027bde4f60f248a0b08eaf7ae0c4dcf16959
tonyassaad <toniassaad@gmail.com>
2012-10-03 22:47:07 +0300

    Update tests/whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -13,9 +13,9 @@ function fgets_u($pStdn) {
 	}
 }
 
-$nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc) 
+$nickname = "WhatsAPI by Tony";
+$sender = 	"916170179590"; // Mobile number with country code (but without + or 00)
+$imei = 	"00:25:D0:B1:26:8C"; // MAC Address for iOS IMEI for other platform (Android/etc) 
 
 
 $countrycode = substr($sender, 0, 2);
